eee09101ea01fd1b3958e308bb9cea75a3333e91
David Biolchini <biolchinid@gmail.com>
2021-06-04 22:54:51 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function(req,res,next) {
+  res.render('form')
+})
+
+
+router.post('/new', function(req,res,next) {
+  const data = req.body
+  messages.push({text: data.message, user: data.name, added: new Date()})
+  res.redirect('/')
+})
+
+module.exports = router;
